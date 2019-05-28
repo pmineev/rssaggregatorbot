05fe6e0cfe8bb92daf9e4a1431ee10136c24357f
Pavel Mineev <mineev96@gmail.com>
2019-05-28 19:46:31 +0300

    improve posts appearance

--- jobs/send_new_posts.py
+++ jobs/send_new_posts.py
@@ -8,7 +8,6 @@ def send_new_posts(bot, job):
     chat_id = job.context
     log.info(f'Sending new posts to {chat_id}')
     posts = bot.database.get_new_posts(chat_id)
-    print(len(posts))
     if posts:
         bot.send_posts(chat_id, posts)
         bot.database.set_last_updated_date(chat_id, int(time.time()))
